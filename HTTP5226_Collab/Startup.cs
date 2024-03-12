ca940d74d291a5a139a7d2c6f0072753751e6fc3
Kyle S <kylearscott@gmail.com>
2024-03-12 13:44:01 -0400

    Initial

---
+++ HTTP5226_Collab/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HTTP5226_Collab.Startup))]
+namespace HTTP5226_Collab
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
